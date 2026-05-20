bb702d3ee929db657b20fdc6b876479580a4f697
Boso <82522285+boso777@users.noreply.github.com>
2026-05-20 12:31:53 +0200

    63

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Laravel\Scout\Searchable;
+use Override;
 
 class Article extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $fillable = [
         'title' , 'description' , 'price' , 'category_id' , 'user_id'
@@ -32,5 +35,15 @@ class Article extends Model
     public static function toBeRevisedCount()
     {
     return Article::where('is_accepted', null)->count();
+    }
+
+    
+    public function toSearchableArray(){
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'category' => $this->category,
+    ];
     }
 }
