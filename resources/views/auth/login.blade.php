fb6cf6d9dfb5e9d12ea0b7ef880e6910fe803e42
Boso <82522285+boso777@users.noreply.github.com>
2026-05-26 10:33:38 +0200

    user story 10 completata

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,16 +6,13 @@
                 @csrf
                 <div class="mb-3">
                     <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                    <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp">
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password">
+                    <input name="password" type="password" class="form-control" id="password">
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
+               
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
